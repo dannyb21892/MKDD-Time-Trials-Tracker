4e827160a1c3fda65eef694daf6896fc2949d839
Daniel Baamonde <dannyb21892@verizon.net>
2019-06-30 15:17:51 -0400

    user data and wrs highlighted on grid

--- src/app/components/standards-grid.component.ts
+++ src/app/components/standards-grid.component.ts
@@ -13,6 +13,7 @@ export class StandardsGrid implements OnInit, OnChanges {
 
   columnDefs = [];
   rowData = [];
+  points = {};
 
   constructor() {}
 
@@ -21,8 +22,12 @@ export class StandardsGrid implements OnInit, OnChanges {
   ngOnChanges(changes: SimpleChanges) {
     Object.keys(changes).forEach(key => {
       this[key] = changes[key].currentValue
-      console.log(`set${this.capitalize(key)}`)
-      this[`set${this.capitalize(key)}`]()
+      console.log(changes)
+      if(key === "standards" && this.wrs && this.userData){
+        this.setStandards()
+      } else if(key !== "standards"){
+        this[`set${this.capitalize(key)}`]()
+      }
     })
   }
 
@@ -34,18 +39,61 @@ export class StandardsGrid implements OnInit, OnChanges {
     let allBuckets = Object.keys(this.standards.standards[Object.keys(this.standards.standards)[0]].fastLap)
     let headers = [...new Set(allBuckets.map(k => k.split(" ")[0].split("+")[0]))]
 
-    this.columnDefs.push({
+    this.columnDefs = [{
       headerName: "Course",
       field: "course",
       rowSpan: function(params) {
         return 2;
       },
-      cellClassRules: { "cell-span": "value.length > 0", "cell-border": "value !== 'LC'"},
-    })
-    this.columnDefs.push({
+      width: 65,
+      cellClassRules: { "cell-span": "value.length > 0", "cell-course-border": "value !== 'LC'", "cell-data-border": "true"},
+      cellStyle: {"text-align": 'center', 'line-height': 3.5, "font-size": '16px'}
+    },{
       headerName: "Trial",
-      field: "trial"
-    })
+      field: "trial",
+      width: 60,
+      cellStyle: {"text-align": 'center'},
+      cellClassRules: { "cell-data-border": "true"}
+    },{
+      headerName: "User Data",
+      children: [{
+        headerName: "PR",
+        field: "time",
+        width: 80,
+        cellStyle: {"text-align": 'center'},
+        cellClassRules: { "cell-data-border": "true"}
+      },{
+        headerName: "Points",
+        field: "points",
+        width: 60,
+        cellStyle: {"text-align": 'center'},
+        cellClassRules: { "cell-data-border": "true"}
+      },{
+        headerName: "Standard",
+        field: "std",
+        width: 80,
+        cellStyle: {"text-align": 'center'},
+        cellClassRules: { "cell-data-border": "true"}
+      },{
+        headerName: "Rank",
+        field: "rank",
+        width: 60,
+        cellStyle: {"text-align": 'center'},
+        cellClassRules: { "cell-data-border": "true"}
+      },{
+        headerName: "PRSR",
+        field: "prsr",
+        width: 60,
+        cellStyle: {"text-align": 'center'},
+        cellClassRules: { "cell-data-border": "true"}
+      },{
+        headerName: "Date",
+        field: "date",
+        width: 100,
+        cellStyle: {"text-align": 'center'},
+        cellClassRules: { "cell-data-border": "true"}
+      }]
+    }]
 
     headers.forEach(h => {
       let children = allBuckets.filter(b => b.includes(h))
@@ -53,19 +101,27 @@ export class StandardsGrid implements OnInit, OnChanges {
         children.sort((a,b) => Number(b.split("+")[1]) - Number(a.split("+")[1]))
       else {
         children.sort()
-        if(children.slice(-1).includes("+"))
+        if(children.slice(-1)[0].includes("+"))
           children = [...children.slice(-1), ...children.slice(0,-1)]
       }
 
+      children.forEach(c => {
+        this.points[c] = this.standards.standards[Object.keys(this.standards.standards)[0]].fastLap[c].points
+      })
+
       let colDef = {
         headerName: h,
-        field: h,
         children: children.map(c => {
-          console.log(allBuckets[c], c, allBuckets)
           return {
-            headerName: c + " (" + this.standards.standards[Object.keys(this.standards.standards)[0]].fastLap[c].points + " pts)",
+            headerName: c + " (" + this.points[c] + ")",
             field: c,
-          }
+            width: 100,
+            cellStyle: {"text-align": 'center'},
+            cellClassRules: { "cell-data-border": "true",
+                              "wr-time": (params) => {return this.wrSelector(params)},
+                              "pr-time": (params) => {return this.prSelector(params)},
+                            }
+            }
         })
       }
 
@@ -81,6 +137,13 @@ export class StandardsGrid implements OnInit, OnChanges {
         course: "",
         trial: "f-lap",
       }
+
+      Object.assign(row1, this.userData[course]["threeLap"])
+      Object.assign(row2, this.userData[course]["fastLap"])
+
+      row1["time"] = this.valueConverter(this.userData[course]["threeLap"].value)
+      row2["time"] = this.valueConverter(this.userData[course]["fastLap"].value)
+
       allBuckets.forEach(b => {
         row1[b] = this.standards.standards[course].threeLap[b].time
         row2[b] = this.standards.standards[course].fastLap[b].time
@@ -92,13 +155,70 @@ export class StandardsGrid implements OnInit, OnChanges {
   }
 
   setWrs = () => {
+    if(this.userData) this.setStandards()
+  }
 
+  wrSelector = params => {
+    let row = params.rowIndex
+    let wr = this.timeConverter(this.wrs[row])
+    let rowTimes = Object.entries(params.data).map(keyVal => {
+      if(!Object.keys(this.points).includes(keyVal[0])) return null
+
+      let out: any[] = [keyVal[1], this.timeConverter(keyVal[1])]
+      return out
+    })
+    .filter(x => x && x[1] >= wr)
+    return params.value === rowTimes[0][0]
+  }
+
+  prSelector = params => {
+    let row = params.rowIndex
+    let pr = this.timeConverter(params.data.time)
+    let rowTimes = Object.entries(params.data).map(keyVal => {
+      if(!Object.keys(this.points).includes(keyVal[0])) return null
+
+      return [keyVal[1], this.timeConverter(keyVal[1])]
+    })
+    .filter(x => x && x[1] >= pr)
+    return params.value === rowTimes[0][0]
   }
 
   setUserData = () => {
-
+    if(this.wrs) this.setStandards()
   }
 
   capitalize = (s: string) => s[0].toUpperCase() + s.slice(1)
+
+  timeConverter = (time) => {
+    let minutes = time.includes("'") ? Number(time.split("'")[0]) : 0
+    let seconds = Number((time.includes("'") ? time.split("'")[1] : time).split('"')[0])
+    let milliseconds: any = time.split('"')[1]
+    milliseconds = Number(milliseconds + "0".repeat(3 - milliseconds.length))
+    let value: any = minutes*60 + seconds + milliseconds/1000
+    return value
+  }
+
+  valueConverter = (val) => {
+    let minutes = Math.floor(val/60)
+    let seconds = Math.floor(val - 60*minutes)
+    let milliseconds: any = Math.ceil(1000*(val - Math.floor(val)))
+    let out = minutes ? `${minutes}'` : ""
+    out += seconds === 0 ? '00"' : (seconds < 10 ? `0${seconds}"` : `${seconds}"`)
+    milliseconds = milliseconds === 0 ? '0' : (milliseconds < 10 ? `00${milliseconds}` : (milliseconds < 100 ? `0${milliseconds}` : `${milliseconds}`))
+    while(milliseconds.length > 1 && milliseconds.slice(-1)==="0"){
+      milliseconds = milliseconds.slice(0,-1)
+    }
+    return out + milliseconds
+  }
+
+  flatten = arr => {
+    let out = []
+    arr.forEach(x => {
+      if(typeof(x) !== "object") return
+
+      out = [...out, ...x]
+    })
+    return out
+  }
 
 }
--- src/app/services/players-page.service.ts
+++ src/app/services/players-page.service.ts
@@ -57,9 +57,23 @@ export class PlayersPageService implements OnInit{
         let courseNames = rows.slice(0,16).map(r => r.split("<")[0])
         this.courseNamesAbbv = courseNames
         let timeFinder = /[0-9]?\'?[0-9]?[0-9]\"[0-9][0-9]?[0-9]?/g
-        let times = [...res.matchAll(timeFinder)].map(x => x[0])
+        let unorderedTimes = [...res.matchAll(timeFinder)].map(x => x[0])
+        let times = []
+        for(let trial = 0; trial <= 32; trial++){
+          stds.forEach((group, gi) => {
+            let offset = gi === stds.length - 1 ? trial : 0
+            let passedIndices = stds.slice(0, gi).reduce((a,b) => a + b.length, 0)
+            let additional = unorderedTimes.slice(32*passedIndices + trial*group.length - offset, 32*passedIndices + (trial + 1)*group.length - offset)
+            if(gi === stds.length - 1){
+              additional = additional.slice(0,-1) //cut off newbie time
+              additional.push(`9'59"999`)
+            }
+            times = [...times, ...additional]
+          })
+        }
 
         let output = {}
+        let numStandards = stds.reduce((a,b) => a + b.length, 0)
         courseNames.forEach((course, ci) => {
           output[course] = {
             threeLap: {},
@@ -68,18 +82,18 @@ export class PlayersPageService implements OnInit{
           stds.forEach((group, gi) => {
             let passedIndices = stds.slice(0, gi).reduce((a,b) => a + b.length, 0)
             group.forEach((std, si) => {
-              let fullTime = times[passedIndices + group.length*2*ci + si]
+              let fullTime = times[numStandards*2*ci + passedIndices + si]
               let fullMinutes = fullTime.includes("'") ? Number(fullTime.split("'")[0]) : 0
               let fullSeconds = Number((fullTime.includes("'") ? fullTime.split("'")[1] : fullTime).split('"')[0])
               let fullMilliseconds = fullTime.split('"')[1]
-              fullMilliseconds = Number(fullMilliseconds +  + "0".repeat(3 - fullMilliseconds.length))
+              fullMilliseconds = Number(fullMilliseconds + "0".repeat(3 - fullMilliseconds.length))
               output[course].threeLap[std] = {
                 points: points[gi][si],
                 time: fullTime,
                 value: fullMinutes*60 + fullSeconds + fullMilliseconds/1000
               }
 
-              let lapTime = times[passedIndices + group.length*2*ci + group.length + si]
+              let lapTime = times[numStandards*(2*ci + 1) + passedIndices + si]
               let lapMinutes = lapTime.includes("'") ? Number(lapTime.split("'")[0]) : 0
               let lapSeconds = Number((lapTime.includes("'") ? lapTime.split("'")[1] : lapTime).split('"')[0])
               let lapMilliseconds = lapTime.split('"')[1]
@@ -92,6 +106,7 @@ export class PlayersPageService implements OnInit{
             })
           })
         })
+        console.log(output)
         return {courses: courseNames, standards: output}
       })
     )
@@ -148,7 +163,7 @@ export class PlayersPageService implements OnInit{
             .map(x => x.split(">")[1].split("<")[0])
         this.courseNames = courseNames
         this.userList[username].data = {}
-        this.courseNames.forEach(c => {
+        this.courseNames.forEach((c, ci) => {
           let htmlSegment = data.split(c)[1].split("<td rowspan='2'>")[0]
           if(htmlSegment.includes("<td>NT</td>")){
             this.userList[username].data[c] = null
@@ -174,20 +189,24 @@ export class PlayersPageService implements OnInit{
                             .split(');" onmouseout="hide();')[0]
                             .split(",")
             }
-            this.userList[username].data[c] = {
+            this.userList[username].data[this.courseNamesAbbv[ci]] = {
               threeLap: threeLapStats ? {
                 value: Number(threeLapStats[1]),
                 rank: Number(threeLapStats[2].slice(2,-1)),
                 std: threeLapStats[3].slice(1,-1),
                 prsr: Number(threeLapStats[5].slice(2,-1)),
-                date: threeLapStats.slice(-1)[0].slice(2,-1)
+                date: threeLapStats.slice(-1)[0].slice(2,-1).split("'")[0],
+                time: threeLapRow.split(";'>")[1].split("<")[0],
+                points: Number(threeLapRow.split(" / ")[0].slice(-3).split(">")[1])
               } : null,
               fastLap: fastLapStats ? {
                 value: Number(fastLapStats[1]),
                 rank: Number(fastLapStats[2].slice(2,-1)),
                 std: fastLapStats[3].slice(1,-1),
                 prsr: Number(fastLapStats[5].slice(2,-1)),
-                date: fastLapStats.slice(-1)[0].slice(2,-1)
+                date: fastLapStats.slice(-1)[0].slice(2,-1).split("'")[0],
+                time: fastLapRow.split(";'>")[1].split("<")[0],
+                points: Number(fastLapRow.split(" / ")[0].slice(-3).split(">")[1])
               } : null
             }
           }
